5616223e664a6341f3746b90ac7ee9808fd534de
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2017-11-15 13:27:24 +0100

    Added `$precision` parameter

--- modules/Files.fdg.php
+++ modules/Files.fdg.php
@@ -1,17 +1,15 @@
 <?php
 class FDG_Files{
-  function sizeToBinary($bytes,$units){
+  function sizeToBinary($bytes,$units,$precision = 2){
     $bytes = max($bytes, 0);
     $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
     $pow = min($pow, count($units) - 1);
-    $precision = 2;
     $bytes /= pow(1024, $pow);;
   }
-  function sizeToSI($bytes,$units){
+  function sizeToSI($bytes,$units,$precision = 2){
     $bytes = max($bytes, 0);
     $pow = floor(($bytes ? log($bytes) : 0) / log(1000));
     $pow = min($pow, count($units) - 1);
-    $precision = 2;
     $bytes /= pow(1000, $pow);;
   }
 }
