c23291cdc22e32ce193a19105bcd9e794a58bed2
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2017-11-15 13:03:02 +0100

    Replace procedural style to OO style

--- modules/Database.fdg.php
+++ modules/Database.fdg.php
@@ -1,12 +1,12 @@
 <?php
 class FDG_Database {
 	public static function Connect($config) {
-    return mysqli_connect($config["Host"], $config["Username"], base64_decode($config["Password"]), $config["Database"]); // open the MySQLi connection
+    return new mysqli($config["Host"], $config["Username"], base64_decode($config["Password"]), $config["Database"]); // open the MySQLi connection
 	}
 
   public static function Close($conn){
     // close the connection and check if it was a success
-    if(mysqli_close($conn)){
+    if($conn->close()){
       // if the connection has been closed
       return true; // return true
     }else{
