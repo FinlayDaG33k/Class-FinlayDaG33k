8d8fdd69502919599741bf2818a8b6c9035b9fa3
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2017-12-12 11:32:07 +0100

    change to static functions

--- modules/Files.fdg.php
+++ modules/Files.fdg.php
@@ -1,12 +1,12 @@
 <?php
 class FDG_Files{
-  function sizeToBinary($bytes,$units,$precision = 2){
+  public static function sizeToBinary($bytes,$units,$precision = 2){
     $bytes = max($bytes, 0);
     $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
     $pow = min($pow, count($units) - 1);
     $bytes /= pow(1024, $pow);;
   }
-  function sizeToSI($bytes,$units,$precision = 2){
+  public static function sizeToSI($bytes,$units,$precision = 2){
     $bytes = max($bytes, 0);
     $pow = floor(($bytes ? log($bytes) : 0) / log(1000));
     $pow = min($pow, count($units) - 1);
