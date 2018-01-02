d5262ec62b72ce518cadcf82351f29a04fddf551
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2018-01-02 23:03:52 +0100

    how about returning the value?
    derp...

--- modules/Files.fdg.php
+++ modules/Files.fdg.php
@@ -1,16 +1,18 @@
 <?php
 class FDG_Files{
-  private $modVersion = "0.1a";
+  private $modVersion = "0.1.1a";
   public static function sizeToBinary($bytes,$units,$precision = 2){
     $bytes = max($bytes, 0);
     $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
     $pow = min($pow, count($units) - 1);
-    $bytes /= pow(1024, $pow);;
+    $bytes /= pow(1024, $pow);
+    return round($bytes, $precision) . ' ' . $units[$pow];
   }
   public static function sizeToSI($bytes,$units,$precision = 2){
     $bytes = max($bytes, 0);
     $pow = floor(($bytes ? log($bytes) : 0) / log(1000));
     $pow = min($pow, count($units) - 1);
-    $bytes /= pow(1000, $pow);;
+    $bytes /= pow(1000, $pow);
+    return round($bytes, $precision) . ' ' . $units[$pow];
   }
 }
