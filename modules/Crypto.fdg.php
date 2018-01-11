e8593fab1a7276528f6a1ad06dee98d1c37c01dc
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2018-01-11 13:19:44 +0100

    Update Crypto.fdg.php

--- modules/Crypto.fdg.php
+++ modules/Crypto.fdg.php
@@ -1,5 +1,7 @@
 <?php
 class FDG_Crypto {
+  private $modVersion = "0.1a";
+  
   public static function genIv($lenght = 16){
     return random_bytes($lenght);
   }
@@ -8,7 +10,7 @@ class FDG_Crypto {
     $iv = self::genIv(16);
     return base64_encode($iv."***".openssl_encrypt($value, "aes-256-cbc", $key, OPENSSL_RAW_DATA, $iv));
   }
-  
+
   public static function aesDecrypt($data,$key){
     $data = explode("***",base64_decode($data));
     $iv = $data[0];
