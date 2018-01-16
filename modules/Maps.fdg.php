339999b331844b9928d8af972e47675e13898c4e
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2018-01-16 07:57:54 +0100

    Made `getDistance` slightly easier to use

--- modules/Maps.fdg.php
+++ modules/Maps.fdg.php
@@ -15,12 +15,12 @@
     * @param string $destination The end location
     * @param string $region the ccTLD of the region you want to search in
     * @param string $mode The mode of traveling
-    * @param int $units Switch between Metric and Imperial: 0 for Metric, 1 for Imperial (who uses Imperial anyways?)
+    * @param int|string $units Switch between Metric and Imperial: 0 for Metric, 1 for Imperial (who uses Imperial anyways?)
     * @param string $key Optional API key for the Google Maps API
     * @return string the JSON response containing both the origin and destination locations and the distance
     */
     public function getDistance($origin,$destination,$region="",$mode="driving",$units=0,$key=""){
-      if($units){
+      if($units || $units = "imperial"){
         $units = "imperial";
       }else{
         $units = "metric";
